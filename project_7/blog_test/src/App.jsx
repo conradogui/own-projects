a923cc37267aba3f472acf2e37cadd4c332fb5aa
conradogui <gui14fbce@gmail.com>
2023-09-29 20:54:31 -0300

    ajustando mensagens

--- project_7/blog_test/src/App.jsx
+++ project_7/blog_test/src/App.jsx
@@ -4,17 +4,23 @@ import './App.css'
 import ToDo from './components/ToDo'
 
 function App() { 
-  const [work, setWork] = useState()
+  const [tarefa, setTarefa] = useState('')
+  const [data, setData] = useState('')
+  const [submittedValue, setSubmittedValue] = useState('')
   
-  const mostra = () => {
-    
+  const mostraTarefa = (e) => {
+    setTarefa(e.target.value)
+  }
+
+  const mostraData = (e) => {
+    setData(e.target.value)
   }
 
   return (
     <div className="app">      
       <div className='container'>
         <div className="add_tarefa">
-          <ToDo mostra={mostra}/>
+            <ToDo mostraTarefa={mostraTarefa} tarefa={tarefa} mostraData={mostraData} data={data} submittedValue={submittedValue} setSubmittedValue={setSubmittedValue} />
         </div>
       </div>
     </div>
--- project_7/blog_test/src/components/ToDo.jsx
+++ project_7/blog_test/src/components/ToDo.jsx
@@ -1,16 +1,23 @@
-const ToDo = ({mostra}) => {
+const ToDo = ({mostraTarefa, tarefa, mostraData, data, submittedValue, setSubmittedValue}) => {
+
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        mostra(mostra)      
+        setSubmittedValue(tarefa, data)
     }
 
   return (
     <div>
         <form action="" onSubmit={handleSubmit} >
-            <input type="text" name="tarefa" id="tarefa"/>
+            <input type="text" name="tarefa" id="tarefa" value={tarefa} onChange={mostraTarefa}/>
+            <input type="date" name="data" id="data" value={data} onChange={mostraData} />            
             <button>Adicionar</button>
         </form>
+        {submittedValue && (
+        <div>
+          Valor submetido: {submittedValue}
+        </div>
+      )}
     </div>
   )
 }
